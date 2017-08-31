c639b4a70b4b68b645c1c0b68478cfa7762555d0
Dragos STOICA <dragos_constantin_stoica@yahoo.com>
2017-08-31 19:12:55 +0200

    Work in progress

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # couchapp-wrapper
 A wrapper for couchapp to sync CouchDB ddoc back and forthe with local repository
+
+# Push2Couch
+Pushc to Couch is a script that allows you to push a flat structure of folders and subfolders into CouchDB as design documents.
+The script has the following input arguments:
+
+```
+--protocol CouchDB server protocol. Default http
+--server   CouchDB server name or IP address. Default localhost
+--port     CouchDB server port. Default 5984
+--user     CouchDB user name
+--password CouchDB password	
+--db       Database name, it must exists. Mandatory argument
+--help     Display help message
+```
+
+The argument for database name `--db` must be supplied. This folder contains the database structure. Each subfolder in the database folder 
+represnts the name of a desing document. Like in the schema bellow:
+
+```
+	test_db
+	      |_ app
+	      |_ user_views
+	      |_ complete_ddoc
+```
+
+This means that we may upload into `test_db` database the following design documents: `_design/app`, `_design/user_views`, `_design/complete_ddoc`.
+The script will delete them if they already exists in the database.
+
+Inside each folder representing a design document the following subforder structure may exists:
+
+```
+	test_db
+	      |_ app
+				|_ attachments
+					|... [JS CouchApp]
+					|... [source files]
+
+				|_ views
+					|_ lib
+						|_ lib_name
+							|_ lib_export_module.js
+					|_ view_name
+						|_ map.js
+						|_ reduce.js
+
+				|_ lists
+					|_ list_function.js
+
+				|_ shows
+					|_ show_function.js
+
+				|_ updates
+					|_ update_function.js
+
+				|_ filters
+					|_ filter_function.js
+
+				|_ full_text
+					|_ index_name
+						|_ index.js
+
+				|_ rewrites.js
+				|_ validate_doc_update.js
+	      
+```
+
+Each subfolder will be mapped to an attribute of the design document or will attach files to that design document.
---
+++ push2couch.sh
@@ -0,0 +1,302 @@
+#!/bin/bash
+
+#
+# Push to CouchDB design documents stored in a folder on local drive.
+# This tool transforms a flat directory-file structure into a couple of
+# design documents, including attachments.
+#
+# @author: Dragos STOICA
+# @date: 15.03.2016
+# @licence: Apache 2.0
+#
+
+
+#
+# Change database connection accordingly
+# using the command line parameters
+#
+COUCHDB_PROTOCOL="http"
+COUCHDB_SERVER="localhost"
+COUCHDB_PORT="5984"
+COUCHDB_USER=""
+COUCHDB_PASSWORD=""
+COUCHDB_CONNECTION=""
+
+COUCHDB_DATABASE=""
+declare -a TMP_FILES=('_head.txt' '_tail.txt'); 
+
+#
+# Auxiliary function to build CouchDB connection string
+#
+build_couchdb_connection()
+{
+	if [[ ${COUCHDB_USER} ]] && [[ ${COUCHDB_PASSWORD} ]]; then
+		COUCHDB_CONNECTION=$COUCHDB_PROTOCOL"://"$COUCHDB_SERVER":"$COUCHDB_PORT
+	else
+		COUCHDB_CONNECTION=$COUCHDB_PROTOCOL"://"$COUCHDB_USER":"$COUCHDB_PASSWORD"@"$COUCHDB_SERVER":"$COUCHDB_PORT
+	fi
+}
+
+#
+# Auxiliary function to delete temporary files
+#
+del_tmp_files()
+{
+	for tmpf in "${TMP_FILES[@]}"
+	do
+		if [ -e "$tmpf" ]; then
+			# echo "File exists"
+			rm $tmpf
+		fi 
+	done
+}
+
+
+#
+# Cleanup first
+# Delete database
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!DO NOT DO THIS IN PRODUCTION!!!
+# !!!THIS IS FOR TESTING PURPOSES!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#curl -X DELETE $COUCHDB_CONNECTION/$2
+#curl -X PUT $COUCHDB_CONNECTION/$2
+
+#
+# Auxiliary function of how to use this tool
+#
+usage()
+{
+	echo "Tool to Push design documents to CouchDB"
+	echo "Usage example:"
+	echo -e "\n$0 --server=[server] --port=[port] --db=[database]\n"
+	echo "All subfolders from folder [database] will be considered as"
+	echo "design documents. The name of the subfolder is the ddoc name "
+	echo -e "\n\t test_db"
+	echo -e "\t\t|_ app"
+	echo -e "\t\t|_ user_views\n"
+	echo "will be mapped to _design/couchapp and _design/user_views."
+	echo ""
+	echo "The structure of the design document is mapped on subfolders:"
+	echo -e "\n\t test_db"
+	echo -e "\t\t|_ app"
+	echo -e "\t\t\t|_ attachments"	
+	echo -e "\t\t\t\t|... [JS CouchApp]"
+	echo -e "\t\t\t\t|... [source files]\n"
+	echo -e "\t\t\t|_ views"	
+	echo -e "\t\t\t\t|_ lib"
+	echo -e "\t\t\t\t\t|_ lib_name"
+	echo -e "\t\t\t\t\t\t|_ lib_export_module.js"
+	echo -e "\t\t\t\t|_ view_name"
+	echo -e "\t\t\t\t\t|_ map.js"
+	echo -e "\t\t\t\t\t|_ reduce.js\n"
+	echo -e "\t\t\t|_ lists"
+	echo -e "\t\t\t\t|_ list_function.js\n"	
+	echo -e "\t\t\t|_ shows"
+	echo -e "\t\t\t\t|_ show_function.js\n"		
+	echo -e "\t\t\t|_ updates"
+	echo -e "\t\t\t\t|_ update_function.js\n"	
+	echo -e "\t\t\t|_ filters"		
+	echo -e "\t\t\t\t|_ filter_function.js\n"	
+	echo -e "\t\t\t|_ full_text"
+	echo -e "\t\t\t\t|_ index_name"
+	echo -e "\t\t\t\t\t|_ index.js\n"	
+	echo -e "\t\t\t|_ rewrites.js"	
+	echo -e "\t\t\t|_ validate_doc_update.js\n"	
+	echo "See test_db folder for an example."
+	echo "The attachments will be uploaded to _design/[ddoc_name]"
+	echo "in the [database]. Please specify other parameters if needed."
+	echo ""
+	echo -e " --protocol\tCouchDB server protocol. Default http"
+	echo -e " --server  \tCouchDB server name or IP address. Default localhost"
+	echo -e " --port    \tCouchDB server port. Default 5984"
+	echo -e " --user    \tCouchDB user name"
+	echo -e " --password\tCouchDB password"	
+	echo -e " --db      \tDatabase name, it must exists. Mandatory argument"
+	echo -e " --help    \tDisplay this message"
+	echo ""
+	echo "> -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~ <"
+	echo "Example:"
+	echo -e "\n$0 --db=test_db\n"
+	echo "Realx and enjoy life!"
+}
+
+
+
+moloz()
+{
+#
+# Cleanup temporary files
+#
+del_tmp_files
+
+
+#
+# Create multipart/realted document
+#
+
+
+FIRST_ATTACHMENT=0
+
+
+echo -en '--5u930\r\ncontent-type: application/json\r\n\r\n' > h.txt
+echo -en '{\r\n\t"pushapp": "v0.0.1",\r\n\t"_attachments": {\r\n' >> h.txt
+
+for file in `find $1 -type f | sed 's:^./\(.*\)$:\1:'`; do
+
+	echo $file
+	if [ $FIRST_ATTACHMENT -eq 0 ]; then
+		FIRST_ATTACHMENT=1
+	else
+		echo -en ",\r\n" >> h.txt
+		 
+	fi
+	
+	MIME_TYPE=`file -b --mime-type ./$file`
+	
+	echo -en '\t\t"'$file'":{\r\n\t\t\t"follows":true,\r\n' >> h.txt
+	echo -en '\t\t\t"content_type":"'$MIME_TYPE'",\r\n' >> h.txt
+    # On linux
+	echo -en '\t\t\t"length":'`stat --printf="%s" ./$file`'\r\n\t\t}' >> h.txt
+    # On MAC
+    #echo -en '\t\t\t"length":'`stat -f "%z" ./$file`'\r\n\t\t}' >> h.txt
+	
+	# Print the content of the file in t.txt
+	echo -en '\r\n--5u930\r\n' >> t.txt
+	if [ "$MIME_TYPE" != "text/plain" ]; then 
+		echo -en 'Content-Type: '$MIME_TYPE >> t.txt
+		echo -en '\r\nContent-transfer-encoding: '`file -b --mime-encoding ./$file`'\r\n\r\n' >> t.txt
+	else
+		echo -en '\r\n' >> t.txt
+	fi
+	cat ./$file >> t.txt
+
+done
+
+echo -en '\r\n\t}\r\n}\r\n' >> h.txt
+
+# Concatenate h and t
+cat t.txt >> h.txt
+
+# Close mulipart/realted document
+echo -en "\r\n--5u930--\r\n" >> h.txt
+
+#
+# Write document to the database
+#
+
+curl -vX PUT $COUCHDB_CONNECTION/$2/$3 \
+-H 'Content-Type:  multipart/related; boundary="5u930"' --data-binary @h.txt
+
+# Cleanup temporary files
+del_tmp_files
+}
+
+
+#
+# The main script
+#
+
+#
+# Check if at leas 2 arguments were passed
+#
+if [ $# -lt 1 ]; then
+	usage
+	exit 0
+fi
+
+#
+# Check command line arguments and exit in the absence of 
+# mandatory arguments
+#
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        --help)
+            usage
+            exit
+            ;;
+        --protocol)
+			COUCHDB_PROTOCOL=$VALUE
+			;;
+        --server)
+            COUCHDB_SERVER=$VALUE
+            ;;
+        --port)
+            COUCHDB_PORT=$VALUE
+            ;;
+        --user)
+            COUCHDB_USER=$VALUE
+            ;;
+        --password)
+            COUCHDB_PASSWORD=$VALUE
+            ;;
+        --db)
+            COUCHDB_DATABASE=$VALUE
+            ;;
+        *)
+			usage
+            echo "ERROR: unknown parameter \"$PARAM\""
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Check database manadatory parameter
+if [ -z "$COUCHDB_DATABASE" ]; then
+	usage
+	echo "Database name must be provided via --db argument"
+	exit 1
+fi
+
+# Check if the folder exits
+if [ ! -e "$COUCHDB_DATABASE" ]; then
+	echo "$COUCHDB_DATABASE folder does not exists!"
+	exit 1
+fi
+
+# Seems ok! Start processing
+build_couchdb_connection
+
+#
+# Go inside the folder and fetch each subfolder
+# this should contain the information for a 
+# desing document
+# 
+for ddoc in `find ${COUCHDB_DATABASE}/ -maxdepth 1 -type d | sed 's/'${COUCHDB_DATABASE}'\///g'`; do
+	echo $ddoc 
+	DDOC_NAME='_design/'$ddoc
+
+	#
+	# Delete destination design document if it exists
+	#
+	REV=`curl -I HEAD -s "$COUCHDB_CONNECTION/$COUCHDB_DATABASE/$DDOC_NAME" | grep "ETag:" | sed 's/ETag: \"/rev=/g' | sed 's/\"//g'`
+
+	if [[ ${REV} ]]; then
+		REV=${REV%$'\r'}
+		curl -vX DELETE  "$COUCHDB_CONNECTION/$COUCHDB_DATABASE/$DDOC_NAME?$REV"
+	fi
+	
+	#
+	# Explore the structure of the desing document folder and 
+	# compose the document
+	#
+	
+	#
+	# Look for attachments subfolder
+	#
+	
+	if [ -e "$DDOC_NAME/attachments" ]; then
+	
+	fi
+	
+	
+done	
+
+exit 5
+
+#
+# End of Main script
+#
